15bb442beb1de8377948007ab9181c63e0477d55
Lunpei Yu <yulunpei0304@icloud.com>
2026-04-06 14:29:44 -0700

    c1: add hello_student program

---
+++ week1/hello_student.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name;
+    string password;
+
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello, " << name << "! Welcome to C++!" << endl;
+
+    cout << "Please enter your password: ";
+    cin >> password;
+    cout << name << "! You should never share your password! (" << password << ")" << endl;
+
+    return 0;
+
+}
